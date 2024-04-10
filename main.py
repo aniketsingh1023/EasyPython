4e707b69a1a836efa62060dc7c6c8c04b745c5fc
aniketsinghn10 <28872440-aniketsinghn10@users.noreply.replit.com>
2024-04-10 15:33:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("when did india get its independence(year)?")
+year = int(input())
+if(year == 1947):
+  print("hip hip hurray. you got that right!")
+else:
+  print("comeon dont you know even this?")
+  print("that's ok, i will let you attempt this once more")
+  print("when did india get its independence?")
+  year = int(input())
+  if(year == 1947):
+    print("you got it!")
